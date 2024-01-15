a9d1387985f05c781ef78b27f082367b23a8c797
Maxens Gagne <mgagne@z1r9p4.42lyon.fr>
2024-01-15 12:49:47 +0100

    cleanup

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 14:23:11 by mgagne            #+#    #+#             */
-/*   Updated: 2024/01/07 19:25:03 by mgagne           ###   ########.fr       */
+/*   Updated: 2024/01/15 12:48:33 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ int main(void)
 	std::string	*stringPTR = &str;
 	std::string	stringREF = str;
 
-	std::cout << &str << std::endl << stringPTR << std::endl << &stringREF << std::endl;
-	std::cout << str << std::endl << *stringPTR << std::endl << stringREF << std::endl;
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return (0);
 }
